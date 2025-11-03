c240a0a78110bb3d33593cd8e0fc06c0b843478a
Fernanda Silva <fer_psilva@outlook.com>
2025-11-03 20:14:40 -0300

    Adiciona CI do PHP

---
+++ tests/CategoriaServiceTest.php
@@ -0,0 +1,11 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use App\Services\CategoriaService;
+
+final class CategoriaServiceTest extends TestCase
+{
+    public function testCreateCategoria()
+    {
+        $this->assertTrue(true); // só pra testar o pipeline
+    }
+}
